47180be577a3e335dca6fb238b2bd27eeb2b7176
Vineet Sahoo <vineetsahoo3@gmail.com>
2026-06-25 13:57:16 +0530

    Fix: Updated the Database configuration

--- backend/credit_score_service/app/database.py
+++ backend/credit_score_service/app/database.py
@@ -3,6 +3,9 @@ import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 logger = logging.getLogger(__name__)
 
